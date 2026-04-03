73eab6ef4e4c7b25a6d99700c831ce6040055e72
Pedro Braga Martins <Pedro@Mac-mini-de-Adriano-5.local>
2026-04-03 12:00:12 -0300

    Corrigindo porta de escuta para o Railway

--- server.js
+++ server.js
@@ -113,4 +113,5 @@ app.delete('/api/exams/:id', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Servidor rodando na porta ${PORT}`));
+// app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
